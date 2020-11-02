58a8b2169409e8a63662436301f6b445c4162edb
Imranul Islam <shoeb1219@gmail.com>
2020-11-02 11:59:34 +0900

    first commit

--- ReportManagement.Web/src/environments/environment.prod.ts
+++ ReportManagement.Web/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://192.168.11.3/api/v1/',
-  apiUrl: 'https://www.moritakegumi.com/api/v1/',
+  // apiUrl: 'https://www.moritakegumi.com/api/v1/',
+   apiUrl: 'https://https://localhost:44330/api/v1',
 };
